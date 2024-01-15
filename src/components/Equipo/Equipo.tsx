b38ec674798f108b5d5ab47051e5fe6f5be6d84e
lucasr <lucasrius86@gmail.com>
2024-01-15 18:10:25 -0300

    Correcciónn ortográfica

--- src/components/Equipo/Equipo.tsx
+++ src/components/Equipo/Equipo.tsx
@@ -57,7 +57,7 @@ export default function App() {
                 <SwiperSlide>
                     <div className="cardEquipo">
                         <div className='imageCardEquipo'>
-                            <Image width={150} src={damian} alt='guiso' />
+                            <Image width={150} src={damian} alt='Damián Gómez' />
                             <p style={{ fontSize: 43, fontWeight: 'bolder', color: '#555555' }}>Damián <span style={{ color: "#00B6AF" }}>Gómez</span></p>
                         </div>
                         <div className='rolContainer'>
@@ -78,7 +78,7 @@ export default function App() {
                 <SwiperSlide>
                     <div className="cardEquipo" id='black'>
                         <div className='imageCardEquipo'>
-                            <Image width={150} src={walter} alt='guiso' />
+                            <Image width={150} src={walter} alt='Walter Gómez' />
                             <p style={{ fontSize: 43, fontWeight: 'bolder', color: '#FFFF' }}>Walter <span style={{ color: "#00B6AF" }}>Gómez</span></p>
                         </div>
                         <div className='rolContainer'>
@@ -121,8 +121,8 @@ export default function App() {
                 <SwiperSlide>
                     <div className="cardEquipo" >
                         <div className='imageCardEquipo'>
-                            <Image width={150} src={mati} alt='guiso' />
-                            <p style={{ fontSize: 43, fontWeight: 'bolder', color: '#555555' }}>Matias <span style={{ color: "#00B6AF" }}>Hernandez</span></p>
+                            <Image width={150} src={mati} alt='Matías Hernandez' />
+                            <p style={{ fontSize: 43, fontWeight: 'bolder', color: '#555555' }}>Matías <span style={{ color: "#00B6AF" }}>Hernandez</span></p>
                         </div>
                         <div className='rolContainer'>
                             <div style={{ marginTop: 10 }}>
@@ -144,14 +144,14 @@ export default function App() {
                 <SwiperSlide>
                     <div className="cardEquipo" id='green'>
                         <div className='imageCardEquipo'>
-                            <Image width={150} src={guille} alt='guiso' />
-                            <p style={{ fontSize: 43, fontWeight: 'bolder', color: '#555555' }}>Guillermina <span style={{ color: "#FFFF" }}>Diaz</span></p>
+                            <Image width={150} src={guille} alt='Guillermina Díaz' />
+                            <p style={{ fontSize: 43, fontWeight: 'bolder', color: '#555555' }}>Guillermina <span style={{ color: "#FFFF" }}>Díaz</span></p>
                         </div>
                         <div className='rolContainer'>
                             <div style={{ marginTop: 10 }}>
                                 <p className={`${montserrat.className} rol`} style={{ color: "white" }}>Desarrolladora Backend</p>
                                 <hr />
-                                <p className={`${montserrat.className} rolDescription`} style={{ color: "white" }}>Encargada del manejo de informacion y estructura de Bases de Datos</p>
+                                <p className={`${montserrat.className} rolDescription`} style={{ color: "white" }}>Encargada del manejo de información y estructura de Bases de Datos</p>
                             </div>
                             <div style={{ marginTop: 20 }}>
                                 <Button as={Link} href='https://www.linkedin.com/in/guillerminaadiaz/' target='_blank' variant='solid' color='primary'>
@@ -173,12 +173,12 @@ export default function App() {
                             <div style={{ marginTop: 10 }}>
                                 <p className={`${montserrat.className} rol`} style={{ color: "white" }}>Desarrollador Backend</p>
                                 <hr />
-                                <p className={`${montserrat.className} rolDescription`} style={{ color: "white" }}>Encargado del manejo de informacion y estructura de Bases de Datos</p>
+                                <p className={`${montserrat.className} rolDescription`} style={{ color: "white" }}>Encargado del manejo de información y estructura de Bases de Datos</p>
                             </div>
                             <div style={{ marginTop: 20 }}>
                                 <Button as={Link} href='https://www.linkedin.com/in/lucas-rius/' target='_blank' variant='solid' color='primary'>
                                     <svg width={20} fill="#ffffff" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>Linked</title> <path d="M28.778 1.004h-25.56c-0.008-0-0.017-0-0.027-0-1.199 0-2.172 0.964-2.186 2.159v25.672c0.014 1.196 0.987 2.161 2.186 2.161 0.010 0 0.019-0 0.029-0h25.555c0.008 0 0.018 0 0.028 0 1.2 0 2.175-0.963 2.194-2.159l0-0.002v-25.67c-0.019-1.197-0.994-2.161-2.195-2.161-0.010 0-0.019 0-0.029 0h0.001zM9.9 26.562h-4.454v-14.311h4.454zM7.674 10.293c-1.425 0-2.579-1.155-2.579-2.579s1.155-2.579 2.579-2.579c1.424 0 2.579 1.154 2.579 2.578v0c0 0.001 0 0.002 0 0.004 0 1.423-1.154 2.577-2.577 2.577-0.001 0-0.002 0-0.003 0h0zM26.556 26.562h-4.441v-6.959c0-1.66-0.034-3.795-2.314-3.795-2.316 0-2.669 1.806-2.669 3.673v7.082h-4.441v-14.311h4.266v1.951h0.058c0.828-1.395 2.326-2.315 4.039-2.315 0.061 0 0.121 0.001 0.181 0.003l-0.009-0c4.5 0 5.332 2.962 5.332 6.817v7.855z"></path> </g></svg>
-                                    <h1 className={montserrat.className} style={{ fontSize: 14, fontWeight: 600 }}>Lucas Benjamin Rius</h1>
+                                    <h1 className={montserrat.className} style={{ fontSize: 14, fontWeight: 600 }}>Lucas Benjamín Rius</h1>
 
                                 </Button>
                             </div>
@@ -188,8 +188,8 @@ export default function App() {
                 <SwiperSlide>
                     <div className="cardEquipo">
                         <div className='imageCardEquipo'>
-                            <Image width={150} src={juli} alt='guiso' />
-                            <p style={{ fontSize: 43, fontWeight: 'bolder', color: '#555555' }}>Julián <span style={{ color: "#00B6AF" }}>Rodriguez</span></p>
+                            <Image width={150} src={juli} alt='Julián Rodríguez' />
+                            <p style={{ fontSize: 43, fontWeight: 'bolder', color: '#555555' }}>Julián <span style={{ color: "#00B6AF" }}>Rodríguez</span></p>
                         </div>
                         <div className='rolContainer'>
                             <div style={{ marginTop: 10 }}>
@@ -209,7 +209,7 @@ export default function App() {
                 <SwiperSlide>
                     <div className="cardEquipo" id='green'>
                         <div className='imageCardEquipo'>
-                            <Image width={150} src={manu} alt='guiso' />
+                            <Image width={150} src={manu} alt='Manuel Latorre' />
                             <p style={{ fontSize: 43, fontWeight: 'bolder', color: '#555555' }}>Manuel <span style={{ color: "#FFFF" }}>Latorre</span></p>
                         </div>
                         <div className='rolContainer'>
