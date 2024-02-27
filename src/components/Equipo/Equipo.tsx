60c2d3764390f68e51e625269ca96ac9711feef4
lucasr <lucasrius86@gmail.com>
2024-02-26 23:36:46 -0300

    Corrección de textos

--- src/components/Equipo/Equipo.tsx
+++ src/components/Equipo/Equipo.tsx
@@ -19,14 +19,27 @@ import './Equipo.css'
 import 'swiper/css';
 import 'swiper/css/pagination';
 import Image from 'next/image';
-import guiso from '../../assets/guiso.png'
-import manu from '../../assets/manu.png'
-import pao from '../../assets/pao.png'
-import juli from '../../assets/juli.png'
-import mati from '../../assets/mati.png'
-import damian from '../../assets/damian.png'
-import guille from '../../assets/guille.png'
-import walter from '../../assets/walter.png'
+// import guiso from '../../assets/guiso.png'
+const guiso = require('../../assets/guiso.png')
+// import manu from '../../assets/manu.png'
+const manu = require('../../assets/manu.png')
+// import pao from '../../assets/pao.png'
+const pao = require('../../assets/pao.png')
+// import juli from '../../assets/juli.png'
+const juli = require('../../assets/juli.png')
+
+// import mati from '../../assets/mati.png'
+const mati = require('../../assets/mati.png')
+
+// import damian from '../../assets/damian.png'
+const damian = require('../../assets/damian.png')
+
+// import guille from '../../assets/guille.png'
+const guille = require('../../assets/guille.png')
+
+// import walter from '../../assets/walter.png'
+const walter = require('../../assets/walter.png')
+
 import Link from 'next/link';
 
 
@@ -64,7 +77,7 @@ export default function App() {
                             <div style={{ marginTop: 10 }}>
                                 <p className={`${montserrat.className} rol`}>Director Operativo</p>
                                 <hr />
-                                <p className={`${montserrat.className} rolDescription`}>Desarrollo las estrategias de comunicación para los canales digitales.</p>
+                                <p className={`${montserrat.className} rolDescription`}>Responsable de supervisar todas las operaciones diarias de la empresa, asegurando que los procesos estén optimizados y que se cumplan los plazos y los estándares de calidad.</p>
                             </div>
                             <div style={{ marginTop: 20 }}>
                                 <Button as={Link} href='https://www.linkedin.com/in/walter-gomez-b97909179/' target='_blank' variant='solid' color='primary'>
@@ -83,9 +96,9 @@ export default function App() {
                         </div>
                         <div className='rolContainer'>
                             <div style={{ marginTop: 10 }}>
-                                <p className={`${montserrat.className} rol`}>Director Operativo</p>
+                                <p className={`${montserrat.className} rol`}>Director Ejecutivo</p>
                                 <hr />
-                                <p className={`${montserrat.className} rolDescription`} style={{ color: "white" }}>Desarrollo las estrategias de comunicación para los canales digitales.</p>
+                                <p className={`${montserrat.className} rolDescription`} style={{ color: "white" }}>Responsable de definir la visión a largo plazo de la empresa y desarrollar estrategias para alcanzar los objetivos de la organización.</p>
                             </div>
                             <div style={{ marginTop: 20 }}>
                                 <Button as={Link} href='https://www.linkedin.com/in/wgsadvise/' target='_blank' variant='solid' color='primary'>
@@ -130,7 +143,7 @@ export default function App() {
                                 <hr />
                                 <p className={`${montserrat.className} rolDescription`}>Colaboro en el desarrollo de soluciones y sitios webs tanto en el Frontend como en el Backend</p>
                             </div>
-                            <div style={{ marginTop: 0}}>
+                            <div style={{ marginTop: 20}}>
                                 <Button as={Link} href='https://www.linkedin.com/in/matiashernandez00/' target='_blank' variant='solid' color='primary'>
                                     <p style={{ color: 'white' }}></p>
                                     <svg width={20} fill="#ffffff" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>Linked</title> <path d="M28.778 1.004h-25.56c-0.008-0-0.017-0-0.027-0-1.199 0-2.172 0.964-2.186 2.159v25.672c0.014 1.196 0.987 2.161 2.186 2.161 0.010 0 0.019-0 0.029-0h25.555c0.008 0 0.018 0 0.028 0 1.2 0 2.175-0.963 2.194-2.159l0-0.002v-25.67c-0.019-1.197-0.994-2.161-2.195-2.161-0.010 0-0.019 0-0.029 0h0.001zM9.9 26.562h-4.454v-14.311h4.454zM7.674 10.293c-1.425 0-2.579-1.155-2.579-2.579s1.155-2.579 2.579-2.579c1.424 0 2.579 1.154 2.579 2.578v0c0 0.001 0 0.002 0 0.004 0 1.423-1.154 2.577-2.577 2.577-0.001 0-0.002 0-0.003 0h0zM26.556 26.562h-4.441v-6.959c0-1.66-0.034-3.795-2.314-3.795-2.316 0-2.669 1.806-2.669 3.673v7.082h-4.441v-14.311h4.266v1.951h0.058c0.828-1.395 2.326-2.315 4.039-2.315 0.061 0 0.121 0.001 0.181 0.003l-0.009-0c4.5 0 5.332 2.962 5.332 6.817v7.855z"></path> </g></svg>
